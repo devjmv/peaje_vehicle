0f681090a44b2f9287a6e51e82a633732be54794
devjmv <javier161890@gmail.com>
2024-09-06 22:38:47 +0200

    aagregado el test para Coche

--- src/test/java/com/javier/VeiculoTest.java
+++ src/test/java/com/javier/VeiculoTest.java
@@ -8,7 +8,6 @@ public class VeiculoTest {
 
     @Test
     void testGetPlaca() {
-        // Usamos Coche para probar el comportamiento de Vehiculo
         Veiculo vehiculo = new Coche("ABC123");
         assertEquals("ABC123", vehiculo.getPlaca(), "La placa del vehículo debe ser ABC123");
     }
