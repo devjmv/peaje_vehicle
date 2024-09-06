e8455ec84d0a67bcd0ff6effc2fd1b10be2a6bf4
devjmv <javier161890@gmail.com>
2024-09-06 22:00:43 +0200

    add clase EstacionPeaje

---
+++ src/main/java/com/javier/EstacionPeaje.java
@@ -0,0 +1,35 @@
+package com.javier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class EstacionPeaje {
+    
+    private String nombre;
+    private String ciudad;
+    private int totalRecaudado;
+    private List<Veiculo> veiculos;
+
+    public EstacionPeaje(String nombre, String ciudad) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.totalRecaudado = 0;
+        this.veiculos = new ArrayList<>();
+    }
+
+    public void registrarVehiculo(Veiculo vehiculo) {
+        int peaje = vehiculo.calcularPeaje();
+        totalRecaudado += peaje;
+        veiculos.add(vehiculo);
+        System.out.println("Vehículo con placa " + vehiculo.getPlaca() + " registrado, peaje: $" + peaje);
+    }
+
+    public void mostrarInforme() {
+        System.out.println("Estación de Peaje: " + nombre + " en " + ciudad);
+        System.out.println("Vehículos registrados:");
+        for (Veiculo vehiculo : veiculos) {
+            System.out.println("- Placa: " + vehiculo.getPlaca() + ", Peaje: $" + vehiculo.calcularPeaje());
+        }
+        System.out.println("Total recaudado: $" + totalRecaudado);
+    }
+}
